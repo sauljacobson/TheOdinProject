53d3e2083d11492677070cce91f77a9fe5753a68
sauljacobson <gruspeseth@gmail.com>
2025-01-01 18:59:09 +0800

    moved css files to public folder

--- NodeJS/MiniMessageBoard/app.js
+++ NodeJS/MiniMessageBoard/app.js
@@ -5,30 +5,15 @@ const indexRouter = require("./routes/indexRouter");
 const app = express();
 const PORT = 3000; 
 
-const messages = [
-    {
-        text: "Hi there!",
-        user: "Amando",
-        added: new Date()
-    }, 
-    {
-        text: "Hello World!",
-        user: "Charles",
-        added: new Date()
-    }
-];
-
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, '.public')))
+app.use(express.static(path.join(__dirname, './public')))
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/new", indexRouter);
-
-
 
 app.listen(PORT, () => {
     console.log(`Server Application listening to localhost:${PORT}`);
