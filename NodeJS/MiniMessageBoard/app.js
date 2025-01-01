03d93ff30f7d93a6e9fc777634b7a6a30b3f7062
sauljacobson <gruspeseth@gmail.com>
2025-01-01 15:44:47 +0800

    implemented viewing and adding posts

--- NodeJS/MiniMessageBoard/app.js
+++ NodeJS/MiniMessageBoard/app.js
@@ -5,14 +5,35 @@ const PORT = 3000;
 
 const messages = [
     {
-        text:
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    }, 
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
 ];
+
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/*", (req, res) => {
-    res.render("index", {message: null}); 
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index", {title: "Mini Messageboard", messages: messages}); 
+})
+
+app.get("/new", (req, res) => {
+    res.render("form");
+})
+
+app.post("/new", (req, res) => {
+    messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+    res.redirect("/");
 })
 
 app.listen(PORT, () => {
